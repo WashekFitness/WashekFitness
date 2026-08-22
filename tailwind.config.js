05846aa24bb7497362bdd1921ad9c20891c78691
WashekFitness <washekfitness@gmail.com>
2026-08-22 16:14:21 -0400

    Rename tailwind config file to tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,18 @@
 ```js
-module.exports = {
+/** @type {import('tailwindcss').Config} */
+
+export default {
+  darkMode: "class",
+
   content: [
     "./index.html",
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
-  darkMode: "class",
+
   theme: {
     extend: {},
   },
+
   plugins: [],
 };
 ```
