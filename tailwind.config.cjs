90643ed409b49c9b7268abab01885add842dbf63
WashekFitness <washekfitness@gmail.com>
2026-08-22 17:28:01 -0400

    Remove darkMode setting from Tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,17 +1,11 @@
-```js
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: "class",
-
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
-
   theme: {
     extend: {},
   },
-
   plugins: [],
 };
-```
