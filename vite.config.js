470a305d2c31d60a1bdbce58ee756644c7bbc623
WashekFitness <washekfitness@gmail.com>
2026-08-22 15:31:24 -0400

    Refactor vite.config.js to simplify configuration

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,6 @@
-```js
-import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  logLevel: 'error',
-  plugins: [
-    react(),
-  ],
+  plugins: [react()],
 })
-```
