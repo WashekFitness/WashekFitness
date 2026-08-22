26247467d4b345c1c694b4610356c67dcab40992
WashekFitness <washekfitness@gmail.com>
2026-08-22 16:08:11 -0400

    Change darkMode configuration to string format

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,13 @@
 ```js
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: ["class"],
+  darkMode: "class",
+
   content: [
     "./index.html",
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
+
   theme: {
     extend: {
       fontFamily: {
